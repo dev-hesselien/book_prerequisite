a65bc09f08b0a24360ee4b46819b2998722f6263
namosdjoro <namosdjoro@emmaus.asso.fr>
2022-01-23 16:58:16 +0100

    test Class

---
+++ TestMerkleTree.py
@@ -0,0 +1,14 @@
+import unittest 
+from Sender import *
+from DataStructure import *
+
+class TestMerkleTree(unittest.TestCase):
+
+    def test_data_structure(self):
+        dataStructure  = DataStructure()
+        data = dataStructure.data('foo')
+        
+        self.assertEqual(['foo'], data)
+
+if __name__ == '__main__':
+    unittest.main()
