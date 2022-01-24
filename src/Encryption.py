502e83e714ec2b71722a3d9fca91401dbe46ceae
namosdjoro <namosdjoro@emmaus.asso.fr>
2022-01-24 12:58:32 +0100

    class Secp256k1 with 3 main method -> get signature, get private_key, get public_key

--- src/Encryption.py
+++ src/Encryption.py
@@ -1,9 +1,18 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePrivateKey
+from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurveSignatureAlgorithm
 class Secp256k1:
-    def create_private_key(self, mnemonic):
+    #         print(str(private_key.public_key().verify())) 
+    def get_signature_from_private_key(self, private_key: EllipticCurvePrivateKey, mnemonic: str)-> bytes:
+        signature = private_key.sign(mnemonic, ec.ECDSA(hashes.SHA256()))
+        return signature
+    def get_private_key(self) -> EllipticCurvePrivateKey:
         private_key = ec.generate_private_key(
             ec.SECP256K1()
         )
-        signature = private_key.sign(mnemonic, ec.ECDSA(hashes.SHA256()))
-        return signature
+        return private_key
+    def get_public_key(self, private_key: EllipticCurvePrivateKey, signature: bytes, data: bytes, signature_algorithm: EllipticCurveSignatureAlgorithm):
+        public_key = private_key.public_key().verify(signature, data, signature_algorithm)
+        return public_key
+    
