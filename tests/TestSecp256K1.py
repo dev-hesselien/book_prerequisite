ed16852edc92beb0d93f485ad107da3217d04f68
namosdjoro <namosdjoro@emmaus.asso.fr>
2022-01-24 12:57:33 +0100

    rewrite test after changes in class implementation

--- tests/TestSecp256K1.py
+++ tests/TestSecp256K1.py
@@ -1,8 +1,17 @@
 import unittest 
 from src.Encryption import *
-class TestMerkleTree(unittest.TestCase):
+class TestSecp256K1c(unittest.TestCase):
     
     def test_generate_private_key(self) :
         keyClass = Secp256k1()
-        test = keyClass.create_private_key(b"hello")
+        privateKey = keyClass.get_private_key()
+        test = keyClass.get_signature_from_private_key(privateKey, b"hello")
         self.assertIsInstance(test, bytes)
+    
+    def test_get_public_key(self):
+        keyClass = Secp256k1()
+    
+    def test_get_private_key(self):
+        keyClass = Secp256k1()
+        private_key = keyClass.get_private_key()
+        number = private_key.private_bytes
