f55186c8b3646155101e021d01fc05e552a893da
namosdjoro <namosdjoro@emmaus.asso.fr>
2022-01-24 20:49:38 +0100

    methods for Secp256K1 ok

--- src/Encryption.py
+++ src/Encryption.py
@@ -3,18 +3,20 @@ from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePrivateKey
 from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurveSignatureAlgorithm
 from cryptography.hazmat.primitives.serialization import Encoding
+from cryptography.hazmat.primitives.serialization import PublicFormat
 class Secp256k1:
-    #         print(str(private_key.public_key().verify())) 
-    def get_signature_from_private_key(self, private_key: EllipticCurvePrivateKey, mnemonic: bytes)-> bytes:
+    
+    def define_signature_from_private_key(self, private_key: EllipticCurvePrivateKey, mnemonic: bytes)-> bytes:
         signature = private_key.sign(mnemonic, ec.ECDSA(hashes.SHA256()))
         return signature
+    
     def get_private_key(self) -> EllipticCurvePrivateKey:
         private_key = ec.generate_private_key(
             ec.SECP256K1()
         )
         return private_key
-    def get_public_key(self, private_key: EllipticCurvePrivateKey):
-        public_key = private_key.public_key().public_bytes(Encoding.PEM, 'OpenSSH')
-        print(public_key.decode('utf-8'))
+    
+    def get_public_key(self, private_key: EllipticCurvePrivateKey) -> bytes:
+        public_key = private_key.public_key().public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo)
         return public_key
     
