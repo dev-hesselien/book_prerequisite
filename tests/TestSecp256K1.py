efe9d51099439c00a3c346128de7658c88dc01d4
namosdjoro <namosdjoro@emmaus.asso.fr>
2022-01-24 11:42:05 +0100

    testing method that create signature to verify private_key

---
+++ tests/TestSecp256K1.py
@@ -0,0 +1,9 @@
+import unittest 
+from src.Encryption import *
+class TestMerkleTree(unittest.TestCase):
+    
+    def test_generate_private_key(self) :
+        keyClass = Secp256k1()
+        test = keyClass.create_private_key(b"hello")
+        print(test)
+        self.assertEqual('yo', test)
