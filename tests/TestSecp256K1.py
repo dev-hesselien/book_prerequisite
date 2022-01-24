9597e22b3b6c7958f0c3dff6ba8a0bd5a98532b8
namosdjoro <namosdjoro@emmaus.asso.fr>
2022-01-24 13:23:31 +0100

    need to fix test to get public key

--- tests/TestSecp256K1.py
+++ tests/TestSecp256K1.py
@@ -10,8 +10,13 @@ class TestSecp256K1c(unittest.TestCase):
     
     def test_get_public_key(self):
         keyClass = Secp256k1()
+        # private key
+        private_key = keyClass.get_private_key()
+        # signature 
+        signature = keyClass.get_signature_from_private_key(private_key, b'hello')
+        public_key = keyClass.get_public_key(private_key)
     
     def test_get_private_key(self):
         keyClass = Secp256k1()
         private_key = keyClass.get_private_key()
-        number = private_key.private_bytes
+        number = private_key
