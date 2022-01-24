70a037dc0257f31b98809516056cb72637f72a96
namosdjoro <namosdjoro@emmaus.asso.fr>
2022-01-24 20:48:48 +0100

    assertions for get_public_key, signature, and get_private_key ok

--- tests/TestSecp256K1.py
+++ tests/TestSecp256K1.py
@@ -2,10 +2,10 @@ import unittest
 from src.Encryption import *
 class TestSecp256K1c(unittest.TestCase):
     
-    def test_generate_private_key(self) :
+    def test_generate_signature_for_private_key(self) :
         keyClass = Secp256k1()
         privateKey = keyClass.get_private_key()
-        test = keyClass.get_signature_from_private_key(privateKey, b"hello")
+        test = keyClass.define_signature_from_private_key(privateKey, b"hello")
         self.assertIsInstance(test, bytes)
     
     def test_get_public_key(self):
@@ -13,10 +13,11 @@ class TestSecp256K1c(unittest.TestCase):
         # private key
         private_key = keyClass.get_private_key()
         # signature 
-        signature = keyClass.get_signature_from_private_key(private_key, b'hello')
+        signature = keyClass.define_signature_from_private_key(private_key, b'hello')
         public_key = keyClass.get_public_key(private_key)
+        self.assertIsInstance(public_key, bytes)
     
     def test_get_private_key(self):
         keyClass = Secp256k1()
         private_key = keyClass.get_private_key()
-        number = private_key
+        self.assertIsInstance(private_key, EllipticCurvePrivateKey)
