5a6a27a88d26cb08b84c7beef2e7b9049333d092
namosdjoro <namosdjoro@emmaus.asso.fr>
2022-01-24 13:23:01 +0100

    orking on get public key

--- src/Encryption.py
+++ src/Encryption.py
@@ -2,9 +2,10 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePrivateKey
 from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurveSignatureAlgorithm
+from cryptography.hazmat.primitives.serialization import Encoding
 class Secp256k1:
     #         print(str(private_key.public_key().verify())) 
-    def get_signature_from_private_key(self, private_key: EllipticCurvePrivateKey, mnemonic: str)-> bytes:
+    def get_signature_from_private_key(self, private_key: EllipticCurvePrivateKey, mnemonic: bytes)-> bytes:
         signature = private_key.sign(mnemonic, ec.ECDSA(hashes.SHA256()))
         return signature
     def get_private_key(self) -> EllipticCurvePrivateKey:
@@ -12,7 +13,8 @@ class Secp256k1:
             ec.SECP256K1()
         )
         return private_key
-    def get_public_key(self, private_key: EllipticCurvePrivateKey, signature: bytes, data: bytes, signature_algorithm: EllipticCurveSignatureAlgorithm):
-        public_key = private_key.public_key().verify(signature, data, signature_algorithm)
+    def get_public_key(self, private_key: EllipticCurvePrivateKey):
+        public_key = private_key.public_key().public_bytes(Encoding.PEM, 'OpenSSH')
+        print(public_key.decode('utf-8'))
         return public_key
     
